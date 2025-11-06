c17e2cb62fef9522c0d7e77429d064785175d0ba
Keith Badger <keithbadger699@gmail.com>
2025-11-06 10:57:16 -0500

    added query nnn functionality

--- lib/Query.py
+++ lib/Query.py
@@ -1,4 +1,24 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel
+from pydantic_core import from_json
+from collections import Counter
+from pyserini.index.lucene import LuceneIndexReader
 
 class Query(BaseModel):
-    contents: str    
+    contents: str
+
+    def get_nnn(self, index_dir):
+        index_reader = LuceneIndexReader(index_dir)
+        analyzed = index_reader.analyze(self.contents)
+        return dict(Counter(analyzed))
+    
+    # Define remaining TF-IDF embedding methods Here
+    
+
+def main():
+    query = Query.model_validate_json("{ \"contents\" : \"Robots are going to 3D imaging\" }")
+    print(query.get_nnn("pyserini_index"))
+
+    # You can add to this to see that you are getting what you expect
+
+if __name__ == "__main__":
+    main()
