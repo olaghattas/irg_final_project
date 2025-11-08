77ce6aa6d9b6165a8267f5038d6dadeea1e0c46f
olaghattas <olaghattas@hotmail.com>
2025-11-08 13:14:56 -0500

    add bnn

--- lib/Query.py
+++ lib/Query.py
@@ -11,13 +11,22 @@ class Query(BaseModel):
         analyzed = index_reader.analyze(self.contents)
         return dict(Counter(analyzed))
     
+    def get_bnn(self, index_dir):
+        index_reader = LuceneIndexReader(index_dir)
+        analyzed = index_reader.analyze(self.contents)
+        return {term: 1 for term in set(analyzed)}
+    
     # Define remaining TF-IDF embedding methods Here
     
 
 def main():
     query = Query.model_validate_json("{ \"contents\" : \"Robots are going to 3D imaging\" }")
     print(query.get_nnn("pyserini_index"))
-
+    print(query.get_bnn("pyserini_index"))
+    
+    query2 = Query.model_validate_json("{ \"contents\" : \"Robots images are going robots to 3D imaging\" }")
+    print(query2.get_nnn("pyserini_index"))
+    print(query2.get_bnn("pyserini_index"))
     # You can add to this to see that you are getting what you expect
 
 if __name__ == "__main__":
