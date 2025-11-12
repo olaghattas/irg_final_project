ca0da2141c49ddf39be1539f126fe92f570c9888
Keith Badger <keithbadger699@gmail.com>
2025-11-12 15:27:18 -0500

    fixed nnn search

--- lib/TF_IDF.py
+++ lib/TF_IDF.py
@@ -10,7 +10,7 @@ class TF_IDF(Index):
         self.dir = dir
         self.reader = LuceneIndexReader(dir)
 
-    def search(self,query_embedding, doc_embedding_type, k):
+    def search(self,query_embedding, doc_embedding_type ,k):
         # This is the naive implementation if it's too slow I can speed it up later
         if doc_embedding_type == 'nnn':
             docs = dict()
@@ -18,15 +18,18 @@ class TF_IDF(Index):
                 posting_list = self.reader.get_postings_list(term[0])
                 for posting in posting_list:
                     if posting.docid in docs.keys():
-                        docs[posting.docid] += posting.tf
+                        docs[posting.docid] += term[1]*posting.tf
                     else:
-                        docs[posting.docid] = posting.tf
-            print([item[0] for item in sorted(docs.items(), key=lambda kv: kv[1], reverse=True)[0:k]])
+                        docs[posting.docid] = term[1]*posting.tf
+            return sorted(docs.items(), key=lambda kv: kv[1], reverse=True)[0:k]
+            # each of the remaining methods should return a list of (docid, score) pairs sorted by score
+        else:
+            return []
 
 def main():
     index = TF_IDF('pyserini_index')
     query = Query.model_validate_json("{ \"contents\" : \"Robots are going to 3D imaging\" }")
-    index.search(query.get_nnn(index.dir), 'nnn', 10)
+    print(index.search(query.get_nnn(index.dir),'nnn', 10))
 
 if __name__ == "__main__":
     main()
