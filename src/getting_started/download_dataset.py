2a36c7f8cdd97de4e09c11dd76fa3e03aa9afe18
mnakash <akashmoniruzzaman@gmail.com>
2025-11-25 12:53:52 -0500

    update dataset download directory
    now dataset will be downloaded in dataset folder

--- src/getting_started/download_dataset.py
+++ src/getting_started/download_dataset.py
@@ -1,12 +1,16 @@
 from datasets import load_dataset
 import os
+from pathlib import Path
+
+project_root = os.path.abspath(os.path.join(Path.cwd(), "..", ".."))
 
 configs = ["query", "corpus_clean", "corpus_s2orc"]
 
 
 # Loop through and download each if not already saved
 for config in configs:
-    save_path = f"LitSearch_{config}"
+    folder_name = f"LitSearch_{config}"
+    save_path = os.path.join(project_root, "dataset", folder_name)
     
     if os.path.exists(save_path):
         print(f" {config} already downloaded at {save_path}, skipping.\n")
