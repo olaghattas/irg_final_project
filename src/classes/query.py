60de9e8ba461a399d68695c34011d67412feefba
mnakash <akashmoniruzzaman@gmail.com>
2025-11-30 00:42:03 -0500

    add lnc method for query

--- src/classes/query.py
+++ src/classes/query.py
@@ -19,6 +19,20 @@ class Query:
         analyzed = index_reader.analyze(self.contents)
         return dict(Counter(analyzed))
     
+    def get_lnc(self, index_dir):
+        # lnc: log-weighted term frequency with cosine normalization
+        index_reader = LuceneIndexReader(index_dir)
+        analyzed = index_reader.analyze(self.contents)
+        term_counts = Counter(analyzed)
+        if not term_counts:
+            return {}
+
+        weights = {term: 1 + np.log10(tf) for term, tf in term_counts.items()}
+        norm = np.linalg.norm(list(weights.values()))
+        if norm == 0:
+            return weights
+        return {term: weight / norm for term, weight in weights.items()}
+    
     def get_bnn(self, index_dir):
         index_reader = LuceneIndexReader(index_dir)
         analyzed = index_reader.analyze(self.contents)
@@ -44,5 +58,9 @@ def main():
     print(query2.get_bnn("indexes/pyserini_index"))
     # You can add to this to see that you are getting what you expect
 
+    query3 = Query("What is the capital of France?")
+    print(query3.get_nnn("indexes/pyserini_index"))
+    print(query3.get_bnn("indexes/pyserini_index"))
+    print(query3.get_lnc("indexes/pyserini_index"))
 if __name__ == "__main__":
     main()
